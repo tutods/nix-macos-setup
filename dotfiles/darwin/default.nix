e8077d8cdce83e2b81c4586e5048ecf0dfbabef0
Daniel Sousa <jdaniel.asousa@gmail.com>
2025-04-18 17:53:48 +0100

    chore: copy other config

--- dotfiles/darwin/default.nix
+++ dotfiles/darwin/default.nix
@@ -1,61 +1,93 @@
-{ pkgs, config, nix-vscode-extensions, ... }:
+{ pkgs, ... }: {
+  imports = [ ./homebrew.nix ./skhd.nix ./yabai.nix ];
 
-# let my-emacs = (import ./emacs.nix { pkgs = pkgs; }); in
-{
-  environment.systemPackages = with pkgs; [
-    binutils # native-comp needs 'as', provided by this
-
-    (ripgrep.override { withPCRE2 = true; })
-    gnutls # for TLS connectivity
-  ];
-
-  homebrew = import ./homebrew.nix // { enable = true; };
-
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-
-  # getting errors with nix's karabiner, so unfortunately I'm running it outside of Nix for now.
-  # services.karabiner-elements.enable = true;
-
-  # nix.package = pkgs.nix;
-
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
-  nixpkgs.config.allowUnfree = true;
-
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true; # default shell on catalina
-  # programs.fish.enable = true;
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
-
-  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
-  system.defaults.dock.autohide = true;
-
-  system.defaults.NSGlobalDomain.InitialKeyRepeat = 20;
-  system.defaults.NSGlobalDomain.KeyRepeat = 2;
-
-  nixpkgs.hostPlatform = "aarch64-darwin";
-
-  nixpkgs.overlays = [ nix-vscode-extensions.overlays.default ];
-
-  system.defaults.CustomUserPreferences = {
-    # "com.google.Chrome" = {
-    #     "NSUserKeyEquivalents" = {
-    #         "Open Location..." = "@d";
-    #     };
-    # };
+  environment = {
+    variables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
   };
 
-  services.skhd.enable = true;
-  # system.keyboard.userKeyMapping = [
-  #     # caps lock to escape
-  #     {
-  #         HIDKeyboardModifierMappingSrc = 30064771129; 
-  #         HIDKeyboardModifierMappingDst = 30064771113;
-  #     }
-  #     # 
-  # ]
+  programs = { zsh.enable = true; };
+
+  services = {
+    # FIXME: driver issues
+    karabiner-elements.enable = false;
+    sketchybar = {
+      enable = false;
+      extraPackages = with pkgs; [ jq gh ];
+    };
+  };
+
+  networking = {
+    knownNetworkServices = [ "Wi-Fi" "Thunderbolt Bridge" ];
+    dns = [ "192.168.178.10" "9.9.9.9" "1.1.1.1" "8.8.8.8" ];
+  };
+
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      sketchybar-app-font
+    ];
+  };
+
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  system = {
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
+
+    defaults = {
+      ".GlobalPreferences"."com.apple.mouse.scaling" = 4.0;
+      spaces.spans-displays = false;
+      universalaccess = {
+        # FIXME: cannot write universal access
+        #reduceMotion = true;
+        #reduceTransparency = true;
+      };
+
+      dock = {
+        autohide = true;
+        autohide-delay = 0.0;
+        autohide-time-modifier = 0.0;
+        orientation = "bottom";
+        dashboard-in-overlay = true;
+        largesize = 85;
+        tilesize = 50;
+        magnification = true;
+        launchanim = false;
+        mru-spaces = false;
+        show-recents = false;
+        show-process-indicators = false;
+        static-only = true;
+      };
+
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        CreateDesktop = false;
+        FXDefaultSearchScope = "SCcf"; # current folder
+        QuitMenuItem = true;
+      };
+
+      NSGlobalDomain = {
+        _HIHideMenuBar = false;
+        AppleFontSmoothing = 0;
+        AppleInterfaceStyle = "Dark";
+        AppleKeyboardUIMode = 3;
+        AppleScrollerPagingBehavior = true;
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        InitialKeyRepeat = 10;
+        KeyRepeat = 2;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSAutomaticWindowAnimationsEnabled = false;
+        NSWindowResizeTime = 0.0;
+        "com.apple.sound.beep.feedback" = 0;
+        "com.apple.trackpad.scaling" = 2.0;
+      };
+    };
+  };
 }
