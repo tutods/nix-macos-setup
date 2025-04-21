cc3ca16458974eaa91ccabd45a3c5f3634563c0a
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2025-04-21 16:52:51 +0100

    feat: add vinegar app

--- hosts/common/darwin/homebrew.nix
+++ hosts/common/darwin/homebrew.nix
@@ -127,6 +127,7 @@
       "Affinity Designer" = 824171161;
       "Affinity Photo" = 824183456;
       "Baking Soda" = 1601151613;
+      "Vinegar" = 1591303229;
       "PhotoBulk" = 537211143;
       "PasteNow" = 1552536109;
       "Xnip" = 1221250572;
